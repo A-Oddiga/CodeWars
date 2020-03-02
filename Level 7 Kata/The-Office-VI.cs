9d2eeb6e0feb708ae4b593f9fa99226bcac65b51
Adel Labadi <blade.mimiga@hotmail.com>
2020-03-02 11:10:44 -0600

    Update The-Office-VI.cs

--- Level 7 Kata/The-Office-VI.cs
+++ Level 7 Kata/The-Office-VI.cs
@@ -6,12 +6,10 @@ public class Kata
      for(int i = 0; i < x.Length; i++)
      {
       if(x[i] == 's' || x[i] == 'S' || x[i] == 'a' || x[i] == 'A' || x[i] == 'b' || x[i] == 'B' || x[i] == 't' || x[i] == 'T'|| x[i] == 'i' || x[i] == 'I'|| x[i] == 'c' || x[i] == 'C'|| x[i] == 'l' || x[i] == 'L') gotit++;
-     }
-           
+     }          
             if (  gotit + val + happ > 22)
             {
                 return "Sabbatical! Boom!";
-
             }
             else
             {
